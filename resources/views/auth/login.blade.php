f2f2d804293767017b611e6c00d9fc4dcf431d21
barman <barmansystemsp@gmail.com>
2024-09-15 12:00:23 +0330

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="fa" dir="rtl" data-topbar-color="brand">
     <head>
         <meta charset="utf-8" />
-        <title>پرسو تجارت | ورود</title>
+        <title>آداک پترو خورشید قشم | ورود</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
