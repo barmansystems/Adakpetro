87cbb88f01bda282d811c92eb92dd71384fc990d
barman <barmansystemsp@gmail.com>
2024-09-15 12:19:26 +0330

    adak-petro-notification

--- app/Notifications/SendMessage.php
+++ app/Notifications/SendMessage.php
@@ -91,7 +91,7 @@ class SendMessage extends Notification
                         "notification" => [
                             "title" => "",
                             "body" => $message,
-                            "icon" => asset('assets/images/logo-sm.png')
+                            "icon" => "https://adakpetro.moshrefiholding.com/assets/images/logo-sm.png"
                         ],
                     ]
                 ]
